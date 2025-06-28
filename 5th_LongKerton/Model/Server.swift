f8265b5af3cd85435a35363dd922d92bcfafa964
taeju <taeju@gmail.com>
2025-06-29 00:56:35 +0900

    Remove all files in JTJ

--- 5th_LongKerton/Model/Server.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Server.swift
-//  5th_LongKerton
-//
-//  Created by Kim Kyengdong on 6/22/25.
-//
-
--- 5th_LongKerton/ViewModel/ServerFunc.swift
+++
@@ -1,7 +0,0 @@
-//
-//  ServerFunc.swift
-//  5th_LongKerton
-//
-//  Created by Kim Kyengdong on 6/23/25.
-//
-
