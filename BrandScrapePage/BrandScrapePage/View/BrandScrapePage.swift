708eaa3274170356e0ec6d20c6a8f8faf278279c
taeju <taeju@gmail.com>
2025-07-04 15:24:52 +0900

    2025.07.04_test

--- BrandScrapePage/BrandScrapePage/View/BrandScrapePage.swift
+++ BrandScrapePage/BrandScrapePage/View/BrandScrapePage.swift
@@ -71,7 +71,6 @@ struct BrandScrapePage: View {
                 .padding(.bottom, 10)
                 .padding(.leading, 230)
                 // MARK: - 카드 + 페이지뷰 + 인디케이터
-                // MARK: - 카드 + 페이지뷰 + 인디케이터
                 VStack {
                     if viewModel.hasNoScrapedBrands {
                         ZStack {
