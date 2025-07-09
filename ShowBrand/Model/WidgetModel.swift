c2d03c044a2c184b5aa79b50fe743f4b2c38c1dd
taeju <taeju@gmail.com>
2025-07-10 02:38:13 +0900

    Widget

--- ShowBrand/Model/WidgetModel.swift
+++ ShowBrand/Model/WidgetModel.swift
@@ -14,40 +14,40 @@ extension BrandRecommendation {
         BrandRecommendation(
             id: 1,
             brandName: "무신사 스탠다드",
-            brandLogoImageName: "mockLogo1",
-            bannerImageName: "mockBanner1",
+            brandLogoImageName: "Logo1_1",
+            bannerImageName: "mockBanner1 1",
             description: "미니멀 무드의 일상복 추천!"
         ),
         BrandRecommendation(
             id: 2,
             brandName: "앤더슨벨",
-            brandLogoImageName: "mockLogo2",
-            bannerImageName: "mockBanner2",
+            brandLogoImageName: "Logo1_1",
+            bannerImageName: "mockBanner1 1",
             description: "감성적인 스트릿 룩을 연출해보세요."
         ),
         BrandRecommendation(
             id: 3,
             brandName: "커버낫",
-            brandLogoImageName: "mockLogo1",
-            bannerImageName: "mockBanner2",
+            brandLogoImageName: "Logo1_1",
+            bannerImageName: "mockBanner1 1",
             description: "편안함과 스타일을 동시에!"
         ),
         BrandRecommendation(
             id: 4,
             brandName: "마르디 메크르디",
-            brandLogoImageName: "mockLogo2",
-            bannerImageName: "mockBanner1",
+            brandLogoImageName: "Logo1_1",
+            bannerImageName: "mockBanner1 1",
             description: "화사한 컬러의 캐주얼 브랜드"
         ),
         BrandRecommendation(
             id: 5,
             brandName: "디스이즈네버댓",
-            brandLogoImageName: "mockLogo1",
-            bannerImageName: "mockBanner2",
+            brandLogoImageName: "Logo1_1",
+            bannerImageName: "mockBanner1 1",
             description: "하이엔드 스트릿 무드로 완성된 룩"
         )
     ]
     
     /// 단일 샘플 항목
-    static let sample = sampleData.first!
+//    static let sample = sampleData.first!
 }
